1ef4f2a8440b629c5a20de120ee03ead57769b65
Luna <luna.zehnder@laptop.nix>
2024-08-11 14:00:40 +0200

    setting up git

--- flake.nix
+++ flake.nix
@@ -11,11 +11,10 @@
   };
 
   outputs = { self, nixpkgs, ... }@inputs: {
-    nixosConfigurations.default = nixpkgs.lib.nixosSystem {
+    nixosConfigurations.laptop = nixpkgs.lib.nixosSystem {
       specialArgs = {inherit inputs;};
       modules = [
-        ./configuration.nix
-        inputs.home-manager.nixosModules.default
+        ./hosts/laptop/configuration.nix
       ];
     };
   };
