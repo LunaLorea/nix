949b431696ecff7825bf184190d455b430f9339d
Luna <luna.zehnder@laptop.nix>
2024-08-12 13:26:28 +0200

    asd

--- hosts/laptop/modules/home-manager/pass.nix
+++ hosts/laptop/modules/home-manager/pass.nix
@@ -3,7 +3,10 @@
 {
   programs.password-store = {
     enable = true;
-    package = pkgs.pass.withExtensions (exts: [ exts.pass-import, exts.pass-otp  ]);
+    package = pkgs.pass.withExtensions (exts: [ 
+      exts.pass-import
+      exts.pass-otp  
+    ]);
     settings = {
       PASSWORD_STORE_DIR = "$XDG_DATA_HOME/password-store";
     };
