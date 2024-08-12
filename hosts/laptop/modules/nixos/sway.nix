4e770be673ac127bb3045bbc7e6c0420e79a4172
Luna <luna.zehnder@laptop.nix>
2024-08-12 21:35:32 +0200

    asd

--- hosts/laptop/modules/nixos/sway.nix
+++ hosts/laptop/modules/nixos/sway.nix
@@ -1,26 +1,27 @@
-{ config, pkgs, ... }:
 
-let
-  colors-accent = #529699;
-in
 {
-  wayland.windowManager.sway = {
-    enable = true;
-    config = {
-      colors = {
-        focused = {
-          border = colors-accent;
-          background = "#040811";
-          childBorder = colors-accent;
-          indicator = colors-accent;
-          text = "#ffffff";
-        };
-        focusedInactive = {
-          border = colors-accent;
-          background = "#040811";
-          childBorder = colors-accent;
-          indicator = colors-accent;
-          text = "#ffffff";
+  let
+    colors-accent = #529699;
+  in
+  outputs = { config, pkgs, ... }: {
+    wayland.windowManager.sway = {
+      enable = true;
+      config = {
+        colors = {
+          focused = {
+            border = colors-accent;
+            background = "#040811";
+            childBorder = colors-accent;
+            indicator = colors-accent;
+            text = "#ffffff";
+          };
+          focusedInactive = {
+            border = colors-accent;
+            background = "#040811";
+            childBorder = colors-accent;
+            indicator = colors-accent;
+            text = "#ffffff";
+          };
         };
       };
     };
