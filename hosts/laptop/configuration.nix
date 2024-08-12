529cedd041517a943e4c5147f3f054b14c78d383
Luna <luna.zehnder@laptop.nix>
2024-08-12 13:43:33 +0200

    fix xkb error

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -65,9 +65,9 @@
   };
 
   # Configure keymap in X11
-  services.xserver = {
+  services.xserver.xkb = {
     layout = "ch";
-    xkbVariant = "de_nodeadkeys";
+    Variant = "de_nodeadkeys";
   };
 
   # Enabling Fingerprint reader
