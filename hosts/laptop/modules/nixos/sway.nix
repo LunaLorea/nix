e4679266354b7ef69a51f3f7388b98fc63b35e03
Luna <luna.zehnder@laptop.nix>
2024-08-13 23:24:52 +0200

    ojwfjo

--- hosts/laptop/modules/nixos/sway.nix
+++ hosts/laptop/modules/nixos/sway.nix
@@ -136,6 +136,20 @@ in
         };
       };
     };
+    style = ''
+      * {
+        border: none;
+        border-radius: 0;
+        font-size: 20;
+      }
+      window#waybar {
+        background: #16191C;
+        color: #AAB2BF;
+      }
+      #workspaces button {
+        padding: 0 5px;
+      }
+    ''
   };
 
 
