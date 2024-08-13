40365d79f5a6aed7f5ec82e0d98bbc221f685659
Luna <luna.zehnder@laptop.nix>
2024-08-13 23:54:00 +0200

    comment out bg thats not working

--- hosts/laptop/modules/nixos/sway.nix
+++ hosts/laptop/modules/nixos/sway.nix
@@ -57,7 +57,7 @@ in
       output = {
         eDP-1 = {
           scale = "1";
-          bg = "/home/luna/.background-image.jpg fill";
+          #bg = "/home/luna/.background-image.jpg fill";
         };
       };
 
