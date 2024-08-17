b3b03f9db662e2257740327d4fb14703c18d9e33
Luna <luna.zehnder@laptop.nix>
2024-08-17 18:28:41 +0200

    trhnr

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -11,6 +11,19 @@
       inputs.home-manager.nixosModules.default
     ];
 
+  
+    fonts.packages = with pkgs; [
+        noto-fonts
+        noto-fonts-cjk
+        noto-fonts-emoji
+        liberation_ttf
+        fira-code
+        fira-code-symbols
+        mplus-outline-fonts.githubRelease
+        dina-font
+        proggyfonts
+        nerdfonts
+    ];
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
