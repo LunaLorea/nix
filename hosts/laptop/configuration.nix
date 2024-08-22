da60ffaee4f90a74cf924ea7c92e66393597902b
Luna <luna.zehnder@laptop.nix>
2024-08-22 12:27:55 +0200

    quite boot

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -18,14 +18,48 @@
     ];
   # Bootloader.
   # boot.loader.systemd-boot.enable = false;
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.grub = {
-    enable = true;
-    splashImage = /home/luna/.config/nix/media/splashscreen.png;
-    splashMode = "normal";
-    efiSupport = true;
-    device = "nodev";
+  boot = {
+    loader.efi.canTouchEfiVariables = true;
+    
+    loader.grub = {
+      enable = true;
+      splashImage = /home/luna/.config/nix/media/splashscreen.png;
+      splashMode = "normal";
+      efiSupport = true;
+      device = "nodev";
+    };
+
+    plymouth = {
+      enable = true;
+      theme = "rings";
+      themePackages = with pkgs; [
+        # By default we would install all themes
+        (adi1090x-plymouth-themes.override {
+          selected_themes = [ "rings" ];
+        })
+      ];
+    };
+
+    # Enable "Silent Boot"
+    consoleLogLevel = 0;
+    initrd.verbose = false;
+    kernelParams = [
+      "quiet"
+      "splash"
+      "boot.shell_on_fail"
+      "loglevel=3"
+      "rd.systemd.show_status=false"
+      "rd.udev.log_level=3"
+      "udev.log_priority=3"
+    ];
+    # Hide the OS choice for bootloaders.
+    # It's still possible to open the bootloader list by pressing any key
+    # It will just not appear on screen unless a key is pressed
+    loader.timeout = 0;
   };
+  boot.
+  boot.
+  boot.
   
 
 
