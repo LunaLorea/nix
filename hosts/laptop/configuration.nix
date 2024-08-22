43359652916a98551fb49e74277800c70d41c5ea
Luna <luna.zehnder@laptop.nix>
2024-08-22 15:11:36 +0200

    asd

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -54,6 +54,7 @@
       "rd.systemd.show_status=false"
       "rd.udev.log_level=3"
       "udev.log_priority=3"
+      "plymouth:debug"
     ];
     # Hide the OS choice for bootloaders.
     # It's still possible to open the bootloader list by pressing any key
