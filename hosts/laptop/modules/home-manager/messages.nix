a6c5788cbb9818144067b6f50e26f9911dc9d1ba
Luna <luna.zehnder@laptop.nix>
2024-09-10 14:33:46 +0200

    fix

--- hosts/laptop/modules/home-manager/messages.nix
+++ hosts/laptop/modules/home-manager/messages.nix
@@ -6,7 +6,7 @@
     isDefault = false;
     settings = {
       "browser.startup.homepage" = "https://web.whatsapp.com/|https://web.threema.ch/#!/welcome|https://discord.com/channels/@me|https://web.telegram.org/";
-    }
-  }
+    };
+  };
 }
 
