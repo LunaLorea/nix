e4f3682b394ff57c32a21617b18febae5be31cdb
Luna <luna@laptop>
2024-09-16 00:42:58 +0200

    Add a new profile to my firefox instance which only serves the purpose of opening web apps of chat apps like whatsapp and otherwise hides the ui completly

--- hosts/laptop/modules/home-manager/firefox.nix
+++ hosts/laptop/modules/home-manager/firefox.nix
@@ -5,15 +5,37 @@
     enable = true;
     profiles = {
       default = {
-        id = 0;
-      };
+        name = "default";
+      }; 
       messages = {
         id = 1;
         isDefault = false;
         settings = {
           "browser.startup.homepage" = "https://web.whatsapp.com/|https://web.threema.ch/#!/welcome|https://discord.com/channels/@me|https://web.telegram.org/";
+          "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+          "extensions.activeThemeID" = "firefox-compact-dark@mozilla.org";
+          "sidebar.revamp" = true;
+          "sidebar.verticalTabs" = true;
+          "sidebar.main.tools" = "";
+          "layout.css.prefers-color-scheme.content-override" = 0;
         };
+        userChrome = ''
+                      /* Hide different gui items*/
+                      #nav-bar, 
+                      #navigator-toolbox,
+                      #newtab-button-container  {
+                        visibility: collapse !important;
+                      }
+                    '';
       };
+    };
+  };
+
+  xdg.desktopEntries = {
+    messages = {
+      name = "Messages";
+      exec = "firefox -P messages -no-remote";
+      icon = "/home/luna/.config/nix/media/messages_icon.png";
     };
   };
 }
