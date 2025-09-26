d8e4ad2de8c518de12d3f6bb6649e00d7818c353
Luna <luna.zehnder@laptop.nix>
2025-09-26 13:26:39 +0200

    feat: changed firefox to declaratively install browser addons and set privacy rules

--- modules/firefox/default.nix
+++ modules/firefox/default.nix
@@ -12,6 +12,47 @@
     home-manager.users.${host.userName} = {...}: {
       programs.firefox = {
         enable = true;
+        policies = {
+          DisableTelemetry = true;
+          DisableFirefoxStudies = true;
+          EnableTrackingProtection = {
+            Value= true;
+            Locked = true;
+            Cryptomining = true;
+            Fingerprinting = true;
+          };
+          DisablePocket = true;
+          DisableFirefoxAccounts = true;
+          DisableAccounts = true;
+          ExtensionSettings = {
+            "*".installation_mode = "normal"; # blocks all addons except the ones specified below
+              # uBlock Origin:
+              "uBlock0@raymondhill.net" = {
+                install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+                installation_mode = "force_installed";
+              };
+              # Privacy Badger:
+            "jid1-MnnxcxisBPnSXQ@jetpack" = {
+              install_url = "https://addons.mozilla.org/firefox/downloads/latest/privacy-badger17/latest.xpi";
+              installation_mode = "force_installed";
+            };
+              # 1Password:
+            "{d634138d-c276-4fc8-924b-40a0ea21d284}" = {
+              install_url = "https://addons.mozilla.org/firefox/downloads/latest/1password-x-password-manager/latest.xpi";
+              installation_mode = "force_installed";
+            };
+              # Catppuccin theme
+            "{15cb5e64-94bd-41aa-91cf-751bb1a84972}" = {
+              install_url = "https://addons.mozilla.org/firefox/downloads/latest/catppuccin-macchiato-lavender2/latest.xpi";
+              installation_mode = "force_installed";
+            };
+            # Improve Youtube
+            "{3c6bf0cc-3ae2-42fb-9993-0d33104fdcaf}" = {
+              install_url = "https://addons.mozilla.org/firefox/downloads/latest/youtube-addon/latest.xpi";
+              installation_mode = "force_install";
+            };
+          };
+        };
         profiles = {
           default = {
             name = "default";
@@ -20,7 +61,7 @@
             id = 1;
             isDefault = false;
             settings = {
-              "browser.startup.homepage" = "https://web.whatsapp.com/|https://web.threema.ch/#!/welcome|https://discord.com/channels/@me|https://web.telegram.org/|https://mail.proton.me/";
+              "browser.startup.homepage" = "https://web.whatsapp.com/|https://web.threema.ch/#!/welcome|https://web.telegram.org/|https://mail.proton.me/";
               "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
               "extensions.activeThemeID" = "firefox-compact-dark@mozilla.org";
               "sidebar.revamp" = true;
@@ -43,7 +84,7 @@
       xdg.desktopEntries = {
         messages = {
           name = "Messages";
-          exec = "firefox -P messages -no-remote";
+          exec = "firefox -P messages -no-remote --name Firefox-messages";
           icon = builtins.path {path = ./media/messages_icon.png;};
         };
       };
