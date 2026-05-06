936cc95d29803bb60d80ce94568f2f1d492aeeba
Luna Zehnder <git@lunalorea.ch>
2026-05-06 11:06:18 +0200

    update gtk theme to adhere to new homemanager stateversion

--- home.nix
+++ home.nix
@@ -16,6 +16,9 @@
   # release notes.
   home.stateVersion = "24.05"; # Please read the comment before changing.
 
+  # Adopting new behavior post 26.05
+  gtk.gtk4.theme = null;
+
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 }
