230e06f69345d331dd16b5f4c54f9b61b6214336
Luna Zehnder <git@lunalorea.ch>
2026-09-06 19:01:48 +0200

    change pinentry package in gnupg agent

--- configuration.nix
+++ configuration.nix
@@ -136,7 +136,7 @@
   programs.gnupg.agent = {
     enable = true;
     enableSSHSupport = true;
-    pinentryPackage = pkgs.pinentry-gtk2;
+    pinentryPackage = pkgs.pinentry-gnome3;
   };
 
   # This value determines the NixOS release from which the default
