3426f3afca3ba1d48a2dcdedb267b2a9c154f824
Luna <git@lunalorea.ch>
2026-09-10 03:51:00 +0200

    fix wrong url in substituter

--- configuration.nix
+++ configuration.nix
@@ -36,7 +36,7 @@
     ];
     substituters = [
       "https://nix-community.cachix.org"
-      "https://cache.lorea.dev"
+      "https://cache.lorea.dev/myriorama"
     ];
     trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
